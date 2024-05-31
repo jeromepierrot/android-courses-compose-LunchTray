069d83710d3a2514589c1eadc4a27cda498916f0
jpierrot <je.pierrot@gmail.com>
2024-05-31 12:10:03 +0200

    LunchTrayScreen: add TopAppBar + StartScreen

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -16,40 +16,125 @@
 package com.example.lunchtray
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.lunchtray.ui.OrderViewModel
+import com.example.lunchtray.ui.StartOrderScreen
+import com.example.lunchtray.ui.theme.LunchTrayTheme
 
 enum class LunchTrayScreen (@StringRes val title: Int) {
-    StartOrder(title = R.string.start_order),
+    StartOrder(title = R.string.app_name_short),
     EntreeMenu(title = R.string.choose_entree),
     AccompanimentMenu(title = R.string.choose_accompaniment),
     SideDishMenu(title = R.string.choose_side_dish),
     Checkout(title = R.string.order_checkout)
 }
 
-// TODO: AppBar
+// TopAppBar
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LunchAppBar(
+    currentScreen: LunchTrayScreen,
+    isBackButtonEnabled : Boolean = false,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(currentScreen.title),
+                textAlign = TextAlign.Center
+            ) },
+        navigationIcon = {
+            if(isBackButtonEnabled) {
+                IconButton(
+                    onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back button"
+                    )
+                }
+            }
+        },
+        modifier = modifier
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LunchTrayApp() {
-    // TODO: Create Controller and initialization
+    // NavHostController
+    val navController: NavHostController = rememberNavController()
 
     // Create ViewModel
     val viewModel: OrderViewModel = viewModel()
 
+    val currentScreen = LunchTrayScreen.valueOf(LunchTrayScreen.StartOrder.name)
+
     Scaffold(
         topBar = {
-            // TODO: AppBar
+            LunchAppBar(
+                currentScreen = currentScreen,
+                isBackButtonEnabled = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() },
+                modifier = Modifier
+                    .border(
+                        BorderStroke(width = 2.dp, color = Color.Red),
+                        shape = RoundedCornerShape(size = 32.dp)
+                    )
+            )
         }
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
 
-        // TODO: Navigation host
+        // Navigation host
+        NavHost(
+            navController = navController,
+            startDestination = LunchTrayScreen.StartOrder.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = LunchTrayScreen.StartOrder.name) {
+                StartOrderScreen(
+                    onStartOrderButtonClicked = {
+                    },
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun LunchTrayAppPreview() {
+
+    LunchTrayTheme {
+        LunchTrayApp()
+    }
+
 }
